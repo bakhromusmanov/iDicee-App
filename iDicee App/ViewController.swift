311d08a619ebab66533e7b2ab5313eb5a99f0ce0
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-06-29 18:59:01 +0500

    Create array of image views and pick random element when Roll button is pressed

--- iDicee App/ViewController.swift
+++ iDicee App/ViewController.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+
+    let allDices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        SetRandomDices()
     }
 
-
+    
+    fileprivate func SetRandomDices() {
+        diceImageView1.image = allDices.randomElement()
+        diceImageView2.image = allDices.randomElement()
+    }
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        SetRandomDices()
+    }
+    
 }
 
