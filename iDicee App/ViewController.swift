34b590e83a448dfde74a3e9e92053b31cd6b0a9c
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-06-29 12:25:24 +0500

    Link design to our code

--- iDicee App/ViewController.swift
+++ iDicee App/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
